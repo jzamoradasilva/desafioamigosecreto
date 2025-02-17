bbd26cd569c663204bfaf0fd901beda83673f5cd
jzamora <jzamoradasilva@gmail.com>
2025-02-16 23:52:04 -0300

    se crea funcion que maneja agregar amigos, funcion se llama desde tecla enter

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-//Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto
-//  y lo agregarán a una lista visible al hacer clic en "Adicionar".
-
-//Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
-
-//Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
-
-//Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente 
-// un nombre de la lista y se mostrará en la página.
-
-
-
-let intentos = 0;
 let ListaAmigo = [];
-let SortearAmigo = 0; 
+const nombreInput = document.getElementById("amigo");
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('keydown', function(event) {
+      if (event.key === 'Enter') {
+        agregarAmigo();
+      }
+    });
+});
+
+  
 
 function agregarAmigo (){
-let nombre = document.getElementById("amigo").value;
-ListaAmigo.push(nombre);
-console.log(ListaAmigo);
+    let nombre = nombreInput.value.trim();
+if(nombre == ''){
+    alert("Por favor, inserte un nombre");
+
+}else{
+    ListaAmigo.push(nombre);
+    console.log(ListaAmigo);
+    nombreInput.value = '';
+}
+
 }
