79ec257599f632bf5f2228c8cdfc4d18a25e2d75
jzamora <jzamoradasilva@gmail.com>
2025-02-17 21:51:28 -0300

    ajuste de return y estilo de codigo

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const nombreInput = document.getElementById("amigo");
 const amigosListados = document.getElementById("listaAmigos");
 const resultadoSorteo = document.getElementById("resultado");
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.addEventListener('keydown', function(event) {
-      if (event.key === 'Enter') {
-        agregarAmigo();
-      }
+document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            agregarAmigo();
+        }
     });
 });
 
 
-function mostrarAmigos(){
+function mostrarAmigos() {
     amigosListados.innerHTML = '';
     ListaAmigo.forEach((amigo) => {
         let item = document.createElement('li');
@@ -24,26 +24,25 @@ function mostrarAmigos(){
 
 
 
-function agregarAmigo (){
+function agregarAmigo() {
     let nombre = nombreInput.value.trim();
-if(nombre == ''){
-    alert("Por favor, inserte un nombre");
-
-}else{
+    if (nombre == '') {
+        alert("Por favor, inserte un nombre");
+        return;
+    }
     ListaAmigo.push(nombre);
     nombreInput.value = '';
     mostrarAmigos();
-}
 
 }
 
-function sortearAmigo (){
+function sortearAmigo() {
     if (ListaAmigo.length === 0) {
         alert("No hay amigos en la lista para sortear");
         return;
     }
-    
-    let indiceAleatorio = Math.floor(Math.random()* ListaAmigo.length);
+
+    let indiceAleatorio = Math.floor(Math.random() * ListaAmigo.length);
     let amigoSorteado = ListaAmigo[indiceAleatorio];
 
     resultadoSorteo.innerHTML = `El amigo secreto es ${amigoSorteado}`;
